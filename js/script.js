3273c2d7d52327af4c83ab9531374825a7e8b415
nvaccina <niccolovac@gmail.com>
2023-03-21 17:56:17 +0100

    Calcolo prezzo biglietto e estetica

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
+
 const bottoneGen = document.getElementById('genera');
-const prezzoBiglietto = "0.21";
-let bigliettoIntero = (km * prezzoBiglietto);
+const prezzoBiglietto = 0.21;
+const piccoloSconto = 20;
+const grandeSconto = 40;
+
 
 bottoneGen.addEventListener('click', function(){
+  /*name*/
   const input = document.getElementById('name');
   const output = document.getElementById('output');
   const name = input.value;
   output.innerHTML = name;
   input.value = "";
   
-  const fasciaAge = document.getElementById('age');
-  const age = fasciaAge.value;
-  fasciaAge.value = "";
-  
-  if (fasciaAge === minorenne){
-    bigliettoScontato = (costoBiglietto - (costoBiglietto * 20 / 100));
-  } else if (fasciaAge === over65){
-    bigliettoScontato = (costoBiglietto - (costoBiglietto * 40 / 100));
-  }else{
-    bigliettoIntero;
+  let km = document.getElementById('km').value;
+  let costoBiglietto = (km * prezzoBiglietto);
+
+  const fasciaAge = document.getElementById('age').value;
+  console.log("prima",costoBiglietto);
+
+  if (fasciaAge == "minorenne"){
+    costoBiglietto *= (1 - piccoloSconto / 100);
+  } else if (fasciaAge == "over"){
+    costoBiglietto *= (1 - grandeSconto / 100);
   }
-  
+  console.log("dopo",costoBiglietto);
+
+  const message = `
+  ${costoBiglietto.toFixed(2)}&euro;
+  `
+  document.getElementById("prezzo").innerHTML = message;
+
 })
-
-/*const message = `
-&euro; ${bigliettoIntero.toFixed(2)}`*/
-
-document.getElementById("prezzo").innerHTML = bigliettoScontato || bigliettoIntero;

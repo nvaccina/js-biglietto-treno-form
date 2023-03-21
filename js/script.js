009f4c29605a7afe828e0d81afb55743b536e0e9
nvaccina <niccolovac@gmail.com>
2023-03-21 19:19:40 +0100

    Aggionamento layout, estetica e js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 
 const bottoneGen = document.getElementById('genera');
+const bottoneDelete = document.getElementById('annulla');
 const prezzoBiglietto = 0.21;
 const piccoloSconto = 20;
 const grandeSconto = 40;
 
 
 bottoneGen.addEventListener('click', function(){
-  /*name*/
+
   const input = document.getElementById('name');
   const output = document.getElementById('output');
   const name = input.value;
@@ -17,18 +18,39 @@ bottoneGen.addEventListener('click', function(){
   let costoBiglietto = (km * prezzoBiglietto);
 
   const fasciaAge = document.getElementById('age').value;
-  console.log("prima",costoBiglietto);
+
 
   if (fasciaAge == "minorenne"){
     costoBiglietto *= (1 - piccoloSconto / 100);
+    document.getElementById("offerta").innerHTML = "Biglietto Silver";
   } else if (fasciaAge == "over"){
     costoBiglietto *= (1 - grandeSconto / 100);
+    document.getElementById("offerta").innerHTML = "Biglietto Gold";
+  }else{
+    document.getElementById("offerta").innerHTML = "Biglietto Standard";
   }
-  console.log("dopo",costoBiglietto);
 
   const message = `
   ${costoBiglietto.toFixed(2)}&euro;
   `
   document.getElementById("prezzo").innerHTML = message;
 
+
+  const biglietto = document.getElementById('biglietto');
+  if(biglietto.classList.contains('hide')){
+    biglietto.classList.remove('hide');
+  }
+
+  document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 11) + 1;
+  document.getElementById("codice").innerHTML = Math.floor(Math.random() * 1001) + 1;
+})
+
+bottoneDelete.addEventListener('click', function(){
+
+  if(biglietto.classList.contains('hide')){
+    biglietto.classList.remove('hide');
+  }
+  else{
+    biglietto.classList.add('hide');
+  }
 })
